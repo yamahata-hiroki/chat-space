a1656791fb0cd2efa3e3ff1bf1d19467954d9080
yamahata hiroki <oreryu.hy.49@ezweb.ne.jp>
2021-01-06 18:37:23 +0900

    こっミット

---
+++ app/javascript/channels/index.json.jbuilder
@@ -0,0 +1,4 @@
+json.array! @users do |user|
+  json.カラム  user.カラム
+  json.カラム  user.カラム ...
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  def self.search(input, id)
+    return nil if input == ""
+    User.where(['name LIKE ?', "%#{input}%"] ).where.not(id: id).limit(10)
+  end
 
          has_many :group_users
          has_many :groups, through: :group_users
