c840d2f703d30da74a1b3f4354d1e594301da583
yamahata hiroki <oreryu.hy.49@ezweb.ne.jp>
2021-01-08 15:16:56 +0900

    www

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
+
   def self.search(input, id)
     return nil if input == ""
     User.where(['name LIKE ?', "%#{input}%"] ).where.not(id: id).limit(10)
